97d30eafb936e1299ae1558d2a1cad7d1a323e13
Aman Kartha <amankartha1998@gmail.com>
2025-01-03 16:50:40 -0500

    some stuff

--- UbisoftNext/API/GameTest/Renderer.cpp
+++ UbisoftNext/API/GameTest/Renderer.cpp
@@ -140,6 +140,42 @@
         }
     }
 
+    void RenderWithCamera(const Camera& camera,IRenderable renderable, )
+    {
+
+        float angleRadians = a * (PI / 180.0);
+        float cosAngle = cos(-angleRadians);
+        float sinAngle = sin(-angleRadians);
+
+
+        Vector2 currentPosition = getAttachedGameObject()->GetTransform()->GetPosition();
+
+        Vector2 relativePosition = currentPosition - offset;
+
+
+        Vector2 rotatedPosition = Vector2(cosAngle * (relativePosition.x) - sinAngle * (relativePosition.y),
+            sinAngle * (relativePosition.x) + cosAngle * (relativePosition.y)
+
+        );
+
+        Vector2 scaledPosition = rotatedPosition * zoom;
+
+        Vector2 calculatedPosition = scaledPosition + Vector2(APP_VIRTUAL_WIDTH / 2, APP_VIRTUAL_HEIGHT / 2);
+
+        //TODO DONT DRAW IF NOT ON SCREEN
+
+        sprite->SetScale(sprite->GetScale() * zoom);
+
+        sprite->SetPosition(calculatedPosition.x, calculatedPosition.y);
+
+
+        if (calculatedPosition.x >= 0 && calculatedPosition.x <= APP_VIRTUAL_WIDTH &&
+            calculatedPosition.y >= 0 && calculatedPosition.y <= APP_VIRTUAL_HEIGHT) {
+            sprite->Draw();
+        }
+
+    }
+
 
     
 
